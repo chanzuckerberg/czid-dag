492658f73f846b335851d4cf1b0eb270376e7e25
jshoe <github@jsheu.com>
2018-06-08 10:04:21 -0700

    Tweak again

--- idseq_dag/util/s3.py
+++ idseq_dag/util/s3.py
@@ -16,7 +16,6 @@ import logging
 
 import idseq_dag.util.command as command
 import idseq_dag.util.log as log
-from idseq_dag.util.log import write
 
 # Peak network and storage perf for a typical small instance is saturated by
 # just a few concurrent streams.
@@ -126,7 +125,7 @@ def fetch_from_s3(src,
                         pipe_filter = "| gzip -dc "
                     command_params = "{pipe_filter} > {destination}".format(
                         pipe_filter=pipe_filter, destination=dst)
-                write("command_params: %s" % dst)
+                log.write("command_params: %s" % command_params)
 
                 try:
                     assert allow_s3mi
