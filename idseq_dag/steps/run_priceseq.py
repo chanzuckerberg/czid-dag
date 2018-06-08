ce5329ea65ea32bf32832572d8a14772e509d969
jshoe <github@jsheu.com>
2018-06-08 16:20:45 -0700

    Revert priceseq

---
+++ idseq_dag/steps/run_priceseq.py
@@ -0,0 +1,8 @@
+from idseq_dag.engine.pipeline_step import PipelineStep
+import idseq_dag.util.command as command
+
+
+class PipelineStepRunPriceSeq(PipelineStep):
+    def run(self):
+        for f in self.output_files_local():
+            command.execute("date > %s" % f)
