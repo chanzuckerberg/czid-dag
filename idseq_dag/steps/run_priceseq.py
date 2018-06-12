da1212898375f6f489eae0e41fcc0d948262eeeb
jshoe <github@jsheu.com>
2018-06-12 16:57:42 -0700

    Tweak

--- idseq_dag/steps/run_priceseq.py
+++ idseq_dag/steps/run_priceseq.py
@@ -10,21 +10,18 @@ class PipelineStepRunPriceSeq(PipelineStep):
 
         See: http://derisilab.ucsf.edu/software/price/
         """
-        original_inputs = self.input_files_local[0][0:2]
+        input_files = self.input_files_local[0][0:2]
         output_files = self.output_files_local()
-        is_paired = (len(original_inputs) == 2)
+        is_paired = (len(input_files) == 2)
 
         # PriceSeqFilter determines input type based on extension. It will
         # throw an exception if output extension doesn't match input
         # extension. Set the extensions to the same thing and use ln instead
         # of renaming files.
         file_type = self.additional_attributes.get("file_type", "fastq")
-        input_files = [f"{orig}.{file_type}" for orig in original_inputs]
         price_out = [
-            f"{file}_priceseqfilter_output.{file_type}" for file in input_files
+            f"{f}_priceseqfilter_output.{file_type}" for f in input_files
         ]
-        for orig, file in zip(original_inputs, input_files):
-            command.execute(f"ln {orig} {file}")
 
         params = ["PriceSeqFilter", '-a', '12', '-rnf', '90', '-log', 'c']
         if is_paired:
@@ -44,10 +41,11 @@ class PipelineStepRunPriceSeq(PipelineStep):
             self.fq2fa(price_out[0], output_files[0])
             if is_paired:
                 self.fq2fa(price_out[1], output_files[1])
-            log.write("Finished FASTQ to FASTA conversion.")
-        log.write("Finished running PriceSeqFilter.")
 
     def fq2fa(self, input_fastq, output_fasta):
         # FASTQ to FASTA conversion
+        step = "FASTQ to FASTA conversion"
+        log.write(f"Starting {step}...")
         cmd = f"sed -n '1~4s/^@/>/p;2~4p' <{input_fastq} >{output_fasta}"
         command.execute(cmd)
+        log.write(f"Finished {step}.")
