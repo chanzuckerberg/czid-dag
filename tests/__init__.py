448aec9e8b57b7b8406d9a5757639278bdefef0d
yunfangjuan <yunfang@chanzuckerberg.com>
2018-06-13 15:58:23 -0700

    fix typo

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,7 @@ from .run_bowtie2 import RunBowtie2Test
 from .run_cdhitdup import RunCDHitDupTest
 from .run_lzw import RunLZWTest
 from .run_priceseq import RunPriceSeqTest
-from .run_gsanp_filter import RunGsnapFilterTest
+from .run_gsnap_filter import RunGsnapFilterTest
 import idseq_dag.util.log as log
 log.configure_logger()
 
