155b4f7fbab330c1de88965d43f9931a9360d9a7
Meera <msrinivasan@ip-10-131-2-130.us-west-2.compute.internal>
2018-07-13 21:53:03 +0000

    Added processing for single end reads and tested with the commited dag

--- idseq_dag/steps/run_srst2.py
+++ idseq_dag/steps/run_srst2.py
@@ -10,13 +10,13 @@ class PipelineStepRunSRST2(PipelineStep):
     See: https://github.com/katholt/srst2
     '''
     def run(self):
-        ''' Invoking srst2 '''
-        fwd_rd = self.input_files_local[0][0]
-        rev_rd = self.input_files_local[0][1] 
+        ''' Invoking srst2 ''' 
         db_file_path = fetch_from_s3(self.additional_files["resist_gene_db"], self.output_dir_local)
         min_cov = str(self.additional_attributes['min_cov'])
         n_threads = str(self.additional_attributes['n_threads'])   
         if len(self.input_files_local[0]) == 2:
+            fwd_rd = self.input_files_local[0][0]
+            rev_rd = self.input_files_local[0][1]
             shutil.copy2(fwd_rd, 'R1_001.fastq.gz')
             shutil.copy2(rev_rd, 'R2_001.fastq.gz')
             srst2_params = [
@@ -25,8 +25,12 @@ class PipelineStepRunSRST2(PipelineStep):
                 '--log', '--gene_db', db_file_path
             ]
         else:
-            # TODO: run srst2 with other set of flags
-            pass
+            rd = self.input_files_local[0][0]
+            shutil.copy2(rd, 'rd.fastq.gz')
+            srst2_params = [
+                'srst2', '--input_se', 'rd.fastq.gz', '--min_coverage', min_cov,'--threads', n_threads,
+                '--output', self.output_dir_local, '--log', '--gene_db', db_file_path
+            ]
         command.execute(" ".join(srst2_params))
         shutil.copy2('output__genes__ARGannot_r2__results.txt', self.output_files_local()[0])
 
