a049922f97f3e0314d281199543f2b5a11dbacdb
Meera <msrinivasan@ip-10-131-2-130.us-west-2.compute.internal>
2018-07-16 18:21:39 +0000

    Supports both single and paired reads, runs -- no errors

--- idseq_dag/steps/run_srst2.py
+++ idseq_dag/steps/run_srst2.py
@@ -17,14 +17,16 @@ class PipelineStepRunSRST2(PipelineStep):
         if len(self.input_files_local[0]) == 2:
             fwd_rd = self.input_files_local[0][0]
             rev_rd = self.input_files_local[0][1]
+            command.execute('mv ' + fwd_rd + ' R1_001.fastq.gz')
+            command.execute('mv ' + rev_rd + ' R2_001.fastq.gz')
             srst2_params = [
-                'srst2', '--input_pe', fwd_rd, rev_rd, '--forward', '001', '--reverse', '001',
+                'srst2', '--input_pe', 'R1_001.fastq.gz', 'R2_001.fastq.gz', '--forward', '001', '--reverse', '001',
                 '--min_coverage', min_cov,'--threads', n_threads, '--output', self.output_dir_local+'/output',
                 '--log', '--gene_db', db_file_path
             ]
         else:
             rd = self.input_files_local[0][0]
-            shutil.copy2(rd, 'rd.fastq.gz')
+            command.execute('mv '+ rd + ' rd.fastq.gz')
             srst2_params = [
                 'srst2', '--input_se', 'rd.fastq.gz', '--min_coverage', min_cov,'--threads', n_threads,
                 '--output', self.output_dir_local+'/output', '--log', '--gene_db', db_file_path
