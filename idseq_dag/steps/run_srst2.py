182478393febee41509adb5090efbb4fd15a53bd
Meera <msrinivasan@ip-10-131-2-130.us-west-2.compute.internal>
2018-07-16 20:35:04 +0000

    Addressing PR comments

--- idseq_dag/steps/run_srst2.py
+++ idseq_dag/steps/run_srst2.py
@@ -18,16 +18,16 @@ class PipelineStepRunSRST2(PipelineStep):
             fwd_rd = self.input_files_local[0][0]
             rev_rd = self.input_files_local[0][1]
             # Rename to format srst2 expects
-            command.execute('mv ' + fwd_rd + ' R1_001.fastq.gz')
-            command.execute('mv ' + rev_rd + ' R2_001.fastq.gz')
+            command.execute(f"mv {fwd_rd} _R1_001.fastq.gz")
+            command.execute(f"mv {rev_rd} _R2_001.fastq.gz")
             srst2_params = [
-                'srst2', '--input_pe', 'R1_001.fastq.gz', 'R2_001.fastq.gz', '--forward', '001', '--reverse', '001',
+                'srst2', '--input_pe', '_R1_001.fastq.gz', '_R2_001.fastq.gz', '--forward', '_R1_001', '--reverse', '_R2_001',
                 '--min_coverage', min_cov,'--threads', n_threads, '--output', os.path.join(self.output_dir_local, 'output'),
                 '--log', '--gene_db', db_file_path
             ]
         else:
             rd = self.input_files_local[0][0]
-            command.execute('mv '+ rd + ' rd.fastq.gz')
+            command.execute(f"mv {rd} rd.fastq.gz")
             srst2_params = [
                 'srst2', '--input_se', 'rd.fastq.gz', '--min_coverage', min_cov,'--threads', n_threads,
                 '--output', self.output_dir_local+'/output', '--log', '--gene_db', db_file_path
@@ -37,7 +37,7 @@ class PipelineStepRunSRST2(PipelineStep):
             # Cp to aws fails if output file is empty
             with open('output__fullgenes__ARGannot_r2__results.txt', 'a') as out:
                 out.write('\n')
-            command.execute('mv output__fullgenes__ARGannot_r2__results.txt ' + self.output_files_local()[2])
+            command.execute(f"mv output__fullgenes__ARGannot_r2__results.txt {self.output_files_local()[2]}")
     
     # Inherited method
     def count_reads(self):
