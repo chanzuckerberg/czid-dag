8da5e78b68978d7e1dc0932eb1092e27b35bbe39
Meera <msrinivasan@ip-10-131-2-130.us-west-2.compute.internal>
2018-07-16 18:04:38 +0000

    debugged output file issue

--- idseq_dag/steps/run_srst2.py
+++ idseq_dag/steps/run_srst2.py
@@ -4,24 +4,46 @@ from idseq_dag.engine.pipeline_step import PipelineStep
 from idseq_dag.util.s3 import fetch_from_s3
 import idseq_dag.util.command as command
 
+import os
+import glob
+
 class PipelineStepRunSRST2(PipelineStep):
     '''
     srst2 is used to detect resistance genes.
     See: https://github.com/katholt/srst2
     '''
     def run(self):
-        ''' Invoking srst2 ''' 
+        ''' Invoking srst2 '''
+        print("AT START: CURR DIR")
+        print(os.listdir()) 
+        print("SELF.INPUT_FILES AT START")
+        print((self.input_files))
+        print("SELF.OUTPUT_DIR_LOCAL")
+        print(self.output_dir_local)
+        print(os.listdir(self.output_dir_local))
+        print("SELF.GETCWD()")
+        print(os.getcwd())
+        # files = glob.glob(os.getcwd()+ '/*.txt')
+        # for f in files:
+        #    os.remove(f)
+        # print("AFTER REMOVING ALL FILES: CURR DIR:")
+        # print(os.listdir())
         db_file_path = fetch_from_s3(self.additional_files["resist_gene_db"], self.output_dir_local)
         min_cov = str(self.additional_attributes['min_cov'])
         n_threads = str(self.additional_attributes['n_threads'])   
+        # print("BEFORE RUNNING TOOL: CURRENT DIRECTORY")
+        # print(os.listdir())
+        print("self.input_files_local")
+        print(self.input_files_local)
         if len(self.input_files_local[0]) == 2:
             fwd_rd = self.input_files_local[0][0]
             rev_rd = self.input_files_local[0][1]
-            shutil.copy2(fwd_rd, 'R1_001.fastq.gz')
-            shutil.copy2(rev_rd, 'R2_001.fastq.gz')
+            # command.execute('cp '+ fwd_rd + ' R1_001.fastq.gz')
+            # command.execute('cp ' + rev_rd + ' R2_001.fastq.gz')
+            # print(os.listdir())
             srst2_params = [
-                'srst2', '--input_pe', 'R1_001.fastq.gz', 'R2_001.fastq.gz', '--forward', '001', '--reverse', '001',
-                '--min_coverage', min_cov,'--threads', n_threads, '--output', self.output_dir_local,
+                'srst2', '--input_pe', fwd_rd, rev_rd, '--forward', '001', '--reverse', '001',
+                '--min_coverage', min_cov,'--threads', n_threads, '--output', self.output_dir_local+'/output',
                 '--log', '--gene_db', db_file_path
             ]
         else:
@@ -31,9 +53,17 @@ class PipelineStepRunSRST2(PipelineStep):
                 'srst2', '--input_se', 'rd.fastq.gz', '--min_coverage', min_cov,'--threads', n_threads,
                 '--output', self.output_dir_local, '--log', '--gene_db', db_file_path
             ]
+        print("srst2 command: ")
+        print(" ".join(srst2_params))
         command.execute(" ".join(srst2_params))
-        shutil.copy2('output__genes__ARGannot_r2__results.txt', self.output_files_local()[0])
-
+        # import os
+        print("AFTER RUNNING TOOL: INPUT files")
+        print(self.input_files_local)
+        print("AFTER RUNNING TOOL: OUTPUT DIR")
+        print(self.output_dir_local)
+        print(os.listdir(self.output_dir_local))
+        # command.execute('cp ' + output.log '+ self.output_files_local()[0])
+        # command.execute('cp ' + self.output_dir_local + '/output__genes__ARGannot_r2__results.txt ' + self.output_files_local()[1])
     # Inherited method
     def count_reads(self):
         pass
