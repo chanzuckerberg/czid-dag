227f07f36255f2cd35f4f17202033ba244998dbb
yunfangjuan <yunfang@chanzuckerberg.com>
2018-07-23 13:50:01 -0700

    more comments

--- idseq_dag/steps/run_star.py
+++ idseq_dag/steps/run_star.py
@@ -196,7 +196,7 @@ class PipelineStepRunStar(PipelineStep):
                 read.append(line)
                 for i in range(3):
                     read.append(f.readline())
-            elif line[0] == 62:
+            elif line[0] == 62: # Equivalent to '>', fasta format
                 rid = line.decode('utf-8').split('\t', 1)[0].strip()
                 read.append(line)
                 read.append(f.readline())
