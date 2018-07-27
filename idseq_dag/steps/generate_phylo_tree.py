b64f4424ec1f6fe116087ebebd8ee5efe9371736
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-07-27 10:19:49 -0700

    WIP

--- idseq_dag/steps/generate_phylo_tree.py
+++ idseq_dag/steps/generate_phylo_tree.py
@@ -8,55 +8,46 @@ class PipelineStepGeneratePhyloTree(PipelineStep):
     Generate a phylogenetic tree from the input fasta files using kSNP3:
     http://gensoft.pasteur.fr/docs/kSNP3/01/kSNP3.01%20User%20Guide%20.pdf
     Augment the inputs with a NCBI reference genomes from the same taxid.
-    BELOW TO BE ADAPTED
+    BELOW TO BE IMPLEMENTED
     '''
     def run(self):
-        input_files = self.input_files_local[0][0:2]
+        input_files = self.input_files_local[0]
         output_files = self.output_files_local()
-        is_paired = (len(input_files) == 2)
 
-        # PriceSeqFilter determines input type based on extension. It will
-        # throw an exception if output extension doesn't match input
-        # extension.
-        file_type = self.additional_attributes.get("file_type", "fastq")
-        price_out = [
-            f"{f}_priceseqfilter_output.{file_type}" for f in input_files
-        ]
+        taxid = self.additional_attributes["taxid"]
+        local_ncbi_fastas = get_ncbi_genomes(taxid)
 
-        params = ["PriceSeqFilter", '-a', '12', '-rnf', '90', '-log', 'c']
-        if is_paired:
-            params.extend([
-                '-fp', input_files[0], input_files[1], '-op', price_out[0],
-                price_out[1]
-            ])
-        else:
-            params.extend(['-f', input_files[0], '-o', price_out[0]])
-        if "fasta" not in file_type:  # Default fastq. Explicitly specify fasta.
-            params.extend(['-rqf', '85', '0.98'])
-        cmd = " ".join(params)
-        command.execute(cmd)
-
-        # Run FASTQ to FASTA if needed
-        if file_type != 'fasta' and file_type != 'fa':
-            # Fastq
-            self.fq2fa(price_out[0], output_files[0])
-            if is_paired:
-                self.fq2fa(price_out[1], output_files[1])
-        else:
-            command.execute(f"mv {price_out[0]} {output_files[0]}")
-            if is_paired:
-                command.execute(f"mv {price_out[1]} {output_files[1]}")
+        command.execute(f"echo {local_ncbi_fastas} > {output_files[0]}") # temporary
 
     def count_reads(self):
-        ''' Count reads '''
-        self.should_count_reads = True
-        self.counts_dict[self.name] = count.reads_in_group(self.output_files_local()[0:2])
+        pass
 
     @staticmethod
-    def fq2fa(input_fastq, output_fasta):
-        ''' FASTQ to FASTA conversion '''
-        step = "FASTQ to FASTA conversion"
-        log.write(f"Starting {step}...")
-        cmd = f"sed -n '1~4s/^@/>/p;2~4p' <{input_fastq} >{output_fasta}"
-        command.execute(cmd)
-        log.write(f"Finished {step}.")
+    def get_ncbi_genomes(taxid, n=10):
+        '''
+        Retrieve up to n GenBank reference genomes under taxid.
+        Assumes taxid is species-level.
+        '''
+        categories = ["bacteria", "viral", "fungi", "protozoa"]
+        # additional options in genbank that probably don't need right now:
+        # ["archaea", "plant", 
+        # "vertebrate_mammalian", "vertebrate_other", "invertebrate",
+        # "other", "metagenomes"]
+        for cat in categories:
+            genome_list_path = f"ftp://ftp.ncbi.nih.gov/genomes/genbank/{cat}/assembly_summary.txt"
+            cmd = f"cd {self.output_dir_local}; wget {genome_list_path}; "
+            cmd += "cut -f6,7,20 assembly_summary.txt" # columns: 6 = taxid; 7 = species_taxid, 20 = ftp_path
+            cmd += f" | grep '\\t{taxid}\\t'" # try to find taxid in the species_taxids
+            cdm += f" | head -n {n} | cut -f1,3" # take only top n results
+            genomes = list(filter(None, command.execute_with_output(cmd).split("\n")))
+            if genomes:
+                local_ncbi_fastas = []
+                for line in genomes:
+                    taxid, ftp_path = genomes.split("\t")
+                    ftp_fasta_gz = f"{ftp_path}/{os.path.basename(ftp_path)}_genomic.fna.gz"
+                    local_fasta = f"{self.output_dir_local}/refgenome_taxid_{taxid}.fasta"
+                    command.execute(f"wget -O {local_fasta}.gz {ftp_fasta_gz}")
+                    command.execute(f"gunzip {local_fasta}.gz")
+                    local_ncbi_fastas.append(local_fasta)
+                return local_ncbi_fastas
+        return []
