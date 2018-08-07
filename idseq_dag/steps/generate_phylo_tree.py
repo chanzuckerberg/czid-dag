0612b8697a3c196cc85951830953076baca0521f
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-08-07 14:56:08 -0700

    test

--- idseq_dag/steps/generate_phylo_tree.py
+++ idseq_dag/steps/generate_phylo_tree.py
@@ -35,7 +35,7 @@ class PipelineStepGeneratePhyloTree(PipelineStep):
         input_dir_for_ksnp3 = f"{self.output_dir_local}/inputs_for_ksnp3"
         command.execute(f"mkdir {input_dir_for_ksnp3}")
         for idx, local_file in enumerate(input_files):
-            genome_name = f"{os.path.basename(local_file)[:10]}__{idx}"
+            genome_name = f"{os.path.basename(local_file)}__{idx}"
             command.execute(f"ln -s {local_file} {input_dir_for_ksnp3}/{genome_name}")
 
         # Retrieve Genbank references (full assembled genomes).
