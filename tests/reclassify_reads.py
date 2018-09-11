57a1abae615cb5beeebe482324209de09f91858b
Yun-Fang Juan <yunfang@chanzuckerberg.com>
2018-09-11 16:10:13 -0700

    fix bug

--- tests/reclassify_reads.py
+++ tests/reclassify_reads.py
@@ -11,14 +11,14 @@ from idseq_dag.steps.reclassify_reads import PipelineStepReclassifyReads
 class ReclassifyReadsTest(unittest.TestCase):
 
     def test_step_gsnap(self):
-        runstep = IdseqStepSetup.get_step_object(ReclassifyReadsTest, "refined_gsnap_out", paired=True)
+        runstep = IdseqStepSetup.get_step_object(PipelineStepReclassifyReads, "refined_gsnap_out", paired=True)
         runstep.start()
         runstep.wait_until_finished()
         # Check results
         # Clean up the folder
 
     def test_step_rapsearch2(self):
-        runstep = IdseqStepSetup.get_step_object(ReclassifyReadsTest, "refined_rapsearch2_out", paired=True)
+        runstep = IdseqStepSetup.get_step_object(PipelineStepReclassifyReads, "refined_rapsearch2_out", paired=True)
         runstep.start()
         runstep.wait_until_finished()
         # Check results
