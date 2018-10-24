ff5192ae962eea88305ea80ef7b9e1be1f9008a3
Yun-Fang Juan <yunfang@chanzuckerberg.com>
2018-10-24 13:29:10 -0700

    fix syntax issues

--- idseq_dag/steps/blast_contigs.py
+++ idseq_dag/steps/blast_contigs.py
@@ -11,6 +11,8 @@ import idseq_dag.util.count as count
 import idseq_dag.util.s3 as s3
 import idseq_dag.util.m8 as m8
 
+from idseq_dag.steps.run_assembly import PipelineStepRunAssembly
+
 MIN_REF_FASTA_SIZE = 25
 MIN_ASEEMBLED_CONTIG_SIZE = 25
 
@@ -27,9 +29,9 @@ class PipelineStepBlastContigs(PipelineStep):
         '''
         (align_m8, deduped_m8, hit_summary, orig_counts) = self.input_files_local[0]
         assembled_contig, _assembled_scaffold, bowtie_sam, _contig_stats = self.input_files_local[1]
-        reference_fasta = elf.input_files_local[2][0]
+        reference_fasta = self.input_files_local[2][0]
 
-        (blast_m8, refined_m8, refined_hit_summary, refined_counts, genus_contig_json) = self.output_files_local()
+        (blast_m8, refined_m8, refined_hit_summary, refined_counts, contig_summary_json) = self.output_files_local()
         db_type = self.additional_attributes["db_type"]
         if os.path.getsize(assembled_contig) < MIN_ASEEMBLED_CONTIG_SIZE or \
             os.path.getsize(reference_fasta) < MIN_REF_FASTA_SIZE:
@@ -38,7 +40,7 @@ class PipelineStepBlastContigs(PipelineStep):
                 command.execute(f"cp {deduped_m8} {refined_m8}")
                 command.execute(f"cp {hit_summary} {refined_hit_summary}")
                 command.execute(f"cp {orig_counts} {refined_counts}")
-                command.execute("echo '{}' > " + genus_contig_json)
+                command.execute("echo '{}' > " + contig_summary_json)
                 return
 
         (read_dict, accession_dict, _selected_genera) = m8.summarize_hits(hit_summary)
@@ -49,10 +51,10 @@ class PipelineStepBlastContigs(PipelineStep):
         contig_stats = defaultdict(int)
         PipelineStepRunAssembly.generate_info_from_sam(bowtie_sam, read2contig, contig_stats)
 
-        (updated_read_dict, read2blastm8) = self.update_read_dict(read2contig,
-                                                                  top_entry_m8,
-                                                                  read_dict,
-                                                                  accession_dict)
+        (updated_read_dict, read2blastm8, contig2lineage) = self.update_read_dict(read2contig,
+                                                                                  top_entry_m8,
+                                                                                  read_dict,
+                                                                                  accession_dict)
         self.generate_m8_and_hit_summary(updated_read_dict, read2blastm8,
                                          hit_summary, deduped_m8,
                                          refined_hit_summary, refined_m8)
@@ -71,9 +73,38 @@ class PipelineStepBlastContigs(PipelineStep):
                                              evalue_type, db_type.upper(),
                                              lineage_db, deuterostome_db, refined_counts)
         # generate contig stats at genus/species level
+        contig_taxon_summary = self.generate_taxon_summary(read2contig, contig2lineage, updated_read_dict)
+        with open(contig_summary_json, 'w') as contig_outf:
+            json.dump(contig_taxon_summary, contig_outf)
 
         # Upload additional file
         self.additional_files_to_upload.append(top_entry_m8)
+
+    @staticmethod
+    def generate_taxon_summary(read2contig, contig2lineage, read_dict):
+        # Return an array with
+        # { taxid: , tax_level:, contig_counts: { 'contig_name': <count>, .... } }
+        genus_summary = defaultdict(lambda: defaultdict(int))
+        species_summary = defaultdict(lambda: defaultdict(int))
+        for read_id, read_info in read_dict.items():
+            contig = read2contig.get(read_id)
+            if contig and contig != '*':
+                species_taxid, genus_taxid = contig2lineage[contig]
+                species_summary[species_taxid][contig] += 1
+                genus_summary[genus_taxid][contig] += 1
+            else:
+                # not mapping to a contig
+                species_taxid, genus_taxid = read_info[4:6]
+                species_summary[species_taxid]['*'] += 1
+                genus_summary[genus_taxid]['*'] += 1
+        # constructing the array for output
+        output_array = []
+        for idx, summary in enumerate([species_summary, genus_summary]):
+            tax_level = idx + 1
+            for taxid, contig_counts in summary.items():
+                entry = { 'taxid': taxid, 'tax_level': tax_level, 'contig_counts': contig_counts}
+                output_array.append(entry)
+        return output_array
 
     @staticmethod
     def generate_m8_and_hit_summary(consolidated_dict, read2blastm8,
@@ -106,9 +137,11 @@ class PipelineStepBlastContigs(PipelineStep):
         consolidated_dict = read_dict
         read2blastm8 = {}
         contig2accession = {}
+        contig2lineage = {}
 
         for contig_id, accession_id, _percent_id, _alignment_length, e_value, _bitscore, line in m8.iterate_m8(blast_top_m8):
             contig2accession[contig_id] = (accession_id, line)
+            contig2lineage = accession_dict[accession_id]
 
         for read_id, contig_id in read2contig.items():
             (accession, m8_line) = contig2accession.get(contig_id, (None, None))
@@ -118,7 +151,7 @@ class PipelineStepBlastContigs(PipelineStep):
                 consolidated_dict[read_id][2] = species_taxid
             if m8_line:
                 read2blastm8[read_id] = m8_line
-        return (consolidated_dict, read2blastm8)
+        return (consolidated_dict, read2blastm8, contig2lineage)
 
     @staticmethod
     def run_blast(assembled_contig, reference_fasta, db_type, blast_m8, top_entry_m8):
@@ -129,7 +162,7 @@ class PipelineStepBlastContigs(PipelineStep):
             blast_type = 'prot'
             blast_command = 'blastx'
         command.execute(f"makeblastdb -in {reference_fasta} -dbtype {blast_type} -out {blast_index_path}")
-        command.execute(f"{blast_command} -query {contig} -db {blast_index_path} -out {blast_m8} -outfmt 6 -num_alignments 5 -num_threads 32")
+        command.execute(f"{blast_command} -query {assembled_contig} -db {blast_index_path} -out {blast_m8} -outfmt 6 -num_alignments 5 -num_threads 32")
         # further processing of getting the top m8 entry for each contig.
         PipelineStepBlastContigs.get_top_m8(blast_m8, top_entry_m8)
 
