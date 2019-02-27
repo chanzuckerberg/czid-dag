d982116c0956c33522eeb3ed8cce3725acb00dcd
Yun-Fang Juan <yunfang@chanzuckerberg.com>
2019-02-27 15:39:47 -0800

    loc db

--- idseq_dag/util/dict.py
+++ idseq_dag/util/dict.py
@@ -1,9 +1,12 @@
 import sqlite3
+from enum import IntEnum
 
 DICT_DELIMITER = chr(1) # Delimiter for an array of values
-VALUE_TYPE_SCALAR = 1
-VALUE_TYPE_ARRAY = 2
+class IdSeqDictValue(IntEnum):
+    VALUE_TYPE_SCALAR = 1
+    VALUE_TYPE_ARRAY = 2
 SQLITE_TABLE_NAME = "idseq_dict"
+
 
 class IdSeqDict(object):
     '''
@@ -14,10 +17,10 @@ class IdSeqDict(object):
     def __init__(self, db_path, value_type):
         self.db_path = db_path
         self.value_type = value_type
-        self.db_conn =  sqlite3.connect(db_path, check_same_thread = False) # make it thread safe
+        self.db_conn = sqlite3.connect(db_path, check_same_thread=False) # make it thread safe
         cursor = self.db_conn.cursor()
         cursor.execute(f"CREATE TABLE IF NOT EXISTS #{SQLITE_TABLE_NAME} (dict_key VARCHAR(255) PRIMARY KEY, dict_value text)")
-        self.db_conn.commit();
+        self.db_conn.commit()
 
     def __del__(self):
         self.db_conn.commit()
@@ -26,31 +29,30 @@ class IdSeqDict(object):
     def update(self, key, value):
         cursor = self.db_conn.cursor()
         val = value
-        if self.value_type == VALUE_TYPE_ARRAY:
-            val = DICT_DELIMITER.join(value)
+        if self.value_type == IdSeqDictValue.VALUE_TYPE_ARRAY:
+            val = DICT_DELIMITER.join([str(v) for v in value])
         cursor.execute(f"INSERT OR REPLACE INTO #{SQLITE_TABLE_NAME} VALUES ('#{key}', '#{val}')")
 
     def batch_inserts(self, tuples):
-        def tuple_to_sql_str(tuple):
-            val = tuple[1]
-            if self.value_type == VALUE_TYPE_ARRAY:
-                val = DICT_DELIMITER.join(value)
-            return f"('#{tuple[0]}', '#{val}')"
+        if len(tuples) == 0:
+            return
+        def tuple_to_sql_str(user_tuple):
+            val = user_tuple[1]
+            if self.value_type == IdSeqDictValue.VALUE_TYPE_ARRAY:
+                val = DICT_DELIMITER.join([str(v) for v in val])
+            return f"('#{user_tuple[0]}', '#{val}')"
         value_str = ",".join(map(tuple_to_sql_str, tuples))
         cursor = self.db_conn.cursor()
         cursor.execute(f"INSERT OR REPLACE INTO #{SQLITE_TABLE_NAME} VALUES #{value_str}")
-        self.db_conn.commit();
+        self.db_conn.commit()
 
     def get(self, key):
         cursor = self.db_conn.cursor()
         res = cursor.execute(f"SELECT dict_value FROM idseq_dict where dict_key = '#{key}'")
         v = res.fetchone()
-        if v == None:
-            return
-        else:
-            value = v[0]
-            if self.value_type == VALUE_TYPE_ARRAY:
-                return value.split(DICT_DELIMITER)
-            else:
-                return value
-
+        if v is None:
+            return None
+        value = v[0]
+        if self.value_type == IdSeqDictValue.VALUE_TYPE_ARRAY:
+            return value.split(DICT_DELIMITER)
+        return value
