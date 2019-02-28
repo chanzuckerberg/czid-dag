3ec4b7d49507a4202df8918d8ec41cc514ff9bf4
Yun-Fang Juan <yunfang@chanzuckerberg.com>
2019-02-27 16:01:25 -0800

    fix syntax error

--- idseq_dag/util/dict.py
+++ idseq_dag/util/dict.py
@@ -19,7 +19,7 @@ class IdSeqDict(object):
         self.value_type = value_type
         self.db_conn = sqlite3.connect(db_path, check_same_thread=False) # make it thread safe
         cursor = self.db_conn.cursor()
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS #{SQLITE_TABLE_NAME} (dict_key VARCHAR(255) PRIMARY KEY, dict_value text)")
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {SQLITE_TABLE_NAME} (dict_key VARCHAR(255) PRIMARY KEY, dict_value text)")
         self.db_conn.commit()
 
     def __del__(self):
@@ -31,7 +31,7 @@ class IdSeqDict(object):
         val = value
         if self.value_type == IdSeqDictValue.VALUE_TYPE_ARRAY:
             val = DICT_DELIMITER.join([str(v) for v in value])
-        cursor.execute(f"INSERT OR REPLACE INTO #{SQLITE_TABLE_NAME} VALUES ('#{key}', '#{val}')")
+        cursor.execute(f"INSERT OR REPLACE INTO {SQLITE_TABLE_NAME} VALUES ('{key}', '{val}')")
 
     def batch_inserts(self, tuples):
         if len(tuples) == 0:
@@ -40,15 +40,15 @@ class IdSeqDict(object):
             val = user_tuple[1]
             if self.value_type == IdSeqDictValue.VALUE_TYPE_ARRAY:
                 val = DICT_DELIMITER.join([str(v) for v in val])
-            return f"('#{user_tuple[0]}', '#{val}')"
+            return f"('{user_tuple[0]}', '{val}')"
         value_str = ",".join(map(tuple_to_sql_str, tuples))
         cursor = self.db_conn.cursor()
-        cursor.execute(f"INSERT OR REPLACE INTO #{SQLITE_TABLE_NAME} VALUES #{value_str}")
+        cursor.execute(f"INSERT OR REPLACE INTO {SQLITE_TABLE_NAME} VALUES {value_str}")
         self.db_conn.commit()
 
     def get(self, key):
         cursor = self.db_conn.cursor()
-        res = cursor.execute(f"SELECT dict_value FROM idseq_dict where dict_key = '#{key}'")
+        res = cursor.execute(f"SELECT dict_value FROM idseq_dict where dict_key = '{key}'")
         v = res.fetchone()
         if v is None:
             return None
