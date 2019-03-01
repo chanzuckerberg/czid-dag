9c18bfb73b3a7b13019a144ecb46526a88883b3e
Yun-Fang Juan <yunfang@chanzuckerberg.com>
2019-03-01 14:15:49 -0800

    output

--- idseq_dag/steps/generate_lineage_db.py
+++ idseq_dag/steps/generate_lineage_db.py
@@ -16,8 +16,10 @@ class PipelineStepGenerateLineageDB(PipelineStep):
         aegea batch submit --command="pip install --upgrade git+git://github.com/chanzuckerberg/s3mi.git; cd /mnt; git clone https://github.com/chanzuckerberg/idseq-dag.git; cd idseq-dag;  pip3 install -e .; sed 's/<NCBI_DATE>/2018-12-01/g' templates/lineage_db.json > templates/lineage_db.json.now  idseq_dag --no-versioned-output templates/lineage_db.json.now"  --storage /mnt=1000 --volume-type gp2 --ecr-image idseq_dag --memory 240000 --queue idseq-prod-himem --vcpus 32 --job-role idseq-pipeline
 
         """
-        lineage_csv_gz = self.input_files_local[0][0][0]
+        lineage_csv_gz = self.input_files_local[0][0]
         output_db = self.output_files_local()[0]
+        log.write(f"input: {lineage_csv_gz} output: {output_db}")
+
         lineage_dict = IdSeqDict(output_db, IdSeqDictValue.VALUE_TYPE_ARRAY)
         batch_list = {}
         with gzip.open(lineage_csv_gz, "rt") as gzf:
