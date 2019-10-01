b09753a1291d604bd191111598559c7f7a076a6b
Greg Dingle <gdingle@chanzuckerberg.com>
2019-10-01 16:41:59 -0700

    multi in and out

--- idseq_dag/steps/generate_lz4.py
+++ idseq_dag/steps/generate_lz4.py
@@ -9,19 +9,23 @@ from idseq_dag.engine.pipeline_step import PipelineStep
 class PipelineStepGenerateLZ4(PipelineStep):
 
     def run(self):
-        command.execute(self.get_command())
+        # lz4 is "scalable with multi-cores CPU" so we let it parallelize
+        # itself. See https://github.com/lz4/lz4 .
+        for file_list in self.input_files:
+            for input_file in file_list:
+                if input_file.endswith(('.gz', '.zip', '.lz4')):
+                    log.write(f'Skipping already-compressed file {input_file}')
+                else:
+                    command.execute(self.get_command(input_file))
 
-    def get_command(self):
-        input_file = self.input_files[0][0]
-        output_file = self.output_files_local()[0]
-        log.write(f"input: {input_file} output: {output_file}")
+    def get_command(self, input_file):
+        log.write(f"input: {input_file} output: {input_file}.lz4")
         return command_patterns.SingleCommand(
             cmd="lz4",
             args=[
                 "-9", # max compression
                 "-f", # force overwrite output file
-                input_file,
-                output_file
+                input_file
             ]
         )
 
--- tests/unit/steps/test_generate_lz4.py
+++ tests/unit/steps/test_generate_lz4.py
@@ -8,14 +8,16 @@ from idseq_dag.steps.generate_lz4 import PipelineStepGenerateLZ4
 
 INPUT_FILE = relative_file_path(__file__, 'dummy testfile.txt')
 OUTPUT_FILE = INPUT_FILE + '.lz4'
+INPUT_FILE_GZIP = INPUT_FILE + '.gz'
+OUTPUT_FILE_GZIP = INPUT_FILE_GZIP + '.lz4'
 
 class TestPipelineStepGenerateLZ4(unittest.TestCase):
 
     def setUp(self):
         self.step = PipelineStepGenerateLZ4(
             name='test_generate_lz4',
-            input_files=[[INPUT_FILE]],
-            output_files=[OUTPUT_FILE],
+            input_files=[[INPUT_FILE, INPUT_FILE, INPUT_FILE_GZIP]],
+            output_files=[OUTPUT_FILE, OUTPUT_FILE, OUTPUT_FILE_GZIP],
             output_dir_local='',
             ref_dir_local='',
             output_dir_s3='',
@@ -27,11 +29,15 @@ class TestPipelineStepGenerateLZ4(unittest.TestCase):
 
     def test_get_command(self):
         self.assertEqual(
-            self.step.get_command().as_test_str(),
-            'lz4 -9 -f {} {}'.format(INPUT_FILE, OUTPUT_FILE),
+            self.step.get_command(INPUT_FILE).as_test_str(),
+            'lz4 -9 -f {}'.format(INPUT_FILE),
         )
 
     def test_run(self):
         self.step.run()
+
         self.assertTrue(os.path.exists(OUTPUT_FILE))
         os.remove(OUTPUT_FILE)
+
+        # Skip gzip files
+        self.assertFalse(os.path.exists(OUTPUT_FILE_GZIP))
