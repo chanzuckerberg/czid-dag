333abf0facfd1d0ba1ae392fe1172d9678ed2557
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2019-10-11 15:17:49 -0700

    omit cd hit dup for testing

--- idseq_dag/steps/run_cdhitdup.py
+++ idseq_dag/steps/run_cdhitdup.py
@@ -2,22 +2,26 @@ from idseq_dag.engine.pipeline_step import PipelineStep, InputFileErrors
 import idseq_dag.util.command as command
 import idseq_dag.util.command_patterns as command_patterns
 import idseq_dag.util.count as count
+import idseq_dag.util.log as log
+
+# TODO:  Make this contingent on DAG param.
+OMIT = True
 
 class PipelineStepRunCDHitDup(PipelineStep):
     """ Removes duplicate reads.
 
     ```
-    cd-hit-dup 
+    cd-hit-dup
     -i {input_fasta}
     -o {output_fasta}
     -e 0.05
     -u 70
     ```
 
-    Per the CDHit Documentation, available [here](https://github.com/weizhongli/cdhit/wiki), 
-    this command uses a 0.05 threshold for the number of mismatches - indicating that if 
-    two reads are > 95% similar they will be considered duplicates. It uses only the 
-    first/last 70 bases of each read to do the analysis on sequence similarity. 
+    Per the CDHit Documentation, available [here](https://github.com/weizhongli/cdhit/wiki),
+    this command uses a 0.05 threshold for the number of mismatches - indicating that if
+    two reads are > 95% similar they will be considered duplicates. It uses only the
+    first/last 70 bases of each read to do the analysis on sequence similarity.
     """
     def validate_input_files(self):
         if not count.files_have_min_reads(self.input_files_local[0], 2):
@@ -27,6 +31,16 @@ class PipelineStepRunCDHitDup(PipelineStep):
         ''' Invoking cd-hit-dup '''
         input_fas = self.input_files_local[0]
         output_fas = self.output_files_local()
+        if OMIT:
+            for src, dst in zip(input_fas, output_fas):
+                command.execute(
+                    command_patterns.SingleCommand(
+                        cmd='cp',
+                        args=[src, dst]
+                    )
+                )
+                log.write("INFO:  Omitted cd-hit-dup.")
+                return
         cdhitdup_params = [
             '-i', input_fas[0], '-o', output_fas[0],
             '-e', '0.05', '-u', '70'
