7c70528d7e27f46f9f4beda5cc9fb559f2e0eb2f
Andrey Kislyuk <kislyuk@gmail.com>
2020-06-13 17:04:45 -0700

    WIP

--- tests/unit/test_priceseq.py
+++ tests/unit/test_priceseq.py
@@ -12,5 +12,10 @@ fastq_filename = os.path.join(os.path.dirname(__file__), "fixtures", "reads.fast
 @unittest.skipIf(os.uname().sysname != "Linux", "Skipping test on incompatible platform")
 class TestCountReads(unittest.TestCase):
     def test_run_priceseqfilter(self):
-        with tempfile.NamedTemporaryFile() as tf:
-            PipelineStepRunPriceSeq.run_priceseqfilter(None, [fasta_filename], [tf.name], is_paired=False, file_type="fastq")
+        with tempfile.TemporaryDirectory() as td, tempfile.NamedTemporaryFile() as tf:
+            try:
+                wd = os.getcwd()
+                os.chdir(td)
+                PipelineStepRunPriceSeq.run_priceseqfilter(None, [fasta_filename], [tf.name], is_paired=False, file_type="fastq")
+            finally:
+                os.chdir(wd)
