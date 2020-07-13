d9f80a9c37cfc71ecdb8e855321d2b4832a7cdc0
Andrey Kislyuk <kislyuk@gmail.com>
2020-07-13 12:40:28 -0700

    quick_check_file: Assert return value

--- tests/unit/test_validate_input.py
+++ tests/unit/test_validate_input.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import tempfile
 import unittest
 
@@ -9,21 +8,24 @@ from idseq_dag.exceptions import InsufficientReadsError, InvalidFileFormatError
 fasta_filename = os.path.join(os.path.dirname(__file__), "fixtures", "reads.fasta")
 fastq_filename = os.path.join(os.path.dirname(__file__), "fixtures", "reads.fastq")
 
+
 class TestValidateInput(unittest.TestCase):
-    def test_validate_input(self):
-        PipelineStepRunValidateInput.quick_check_file(None, fasta_filename, is_fastq=False, max_fragments_to_check=100)
-        PipelineStepRunValidateInput.quick_check_file(None, fastq_filename, is_fastq=True, max_fragments_to_check=100)
+    def test_quick_check_file(self):
+        quick_check_file = PipelineStepRunValidateInput.quick_check_file
+        # quick_check_file returns false for multi-line fasta
+        self.assertFalse(quick_check_file(None, fasta_filename, is_fastq=False, max_fragments_to_check=100))
+        self.assertTrue(quick_check_file(None, fastq_filename, is_fastq=True, max_fragments_to_check=100))
         with self.assertRaises(FileNotFoundError):
-            PipelineStepRunValidateInput.quick_check_file(None, "nonexistent", is_fastq=True, max_fragments_to_check=100)
+            quick_check_file(None, "nonexistent", is_fastq=True, max_fragments_to_check=100)
         with tempfile.NamedTemporaryFile() as tf:
             with self.assertRaises(InsufficientReadsError):
-                PipelineStepRunValidateInput.quick_check_file(None, tf.name, is_fastq=True, max_fragments_to_check=100)
+                quick_check_file(None, tf.name, is_fastq=True, max_fragments_to_check=100)
             tf.write(b">@" * 9000)
             tf.flush()
             with self.assertRaises(InvalidFileFormatError):
-                PipelineStepRunValidateInput.quick_check_file(None, tf.name, is_fastq=True, max_fragments_to_check=100)
+                quick_check_file(None, tf.name, is_fastq=True, max_fragments_to_check=100)
             tf.seek(0)
             tf.write(">read\n\n".encode())
             tf.flush()
             with self.assertRaises(InvalidFileFormatError):
-                PipelineStepRunValidateInput.quick_check_file(None, tf.name, is_fastq=True, max_fragments_to_check=100)
+                quick_check_file(None, tf.name, is_fastq=True, max_fragments_to_check=100)
